5c8ee57e168c1e7c19e7fd5e4e895d19d52fc919
ArGul-0 <argul.mail@gmail.com>
2025-05-19 16:24:55 +0300

    First commit

--- Small calculator on Windows Forms/CalculatorForm.Designer.cs
+++ Small calculator on Windows Forms/CalculatorForm.Designer.cs
@@ -30,6 +30,16 @@
         {
             button1 = new Button();
             button2 = new Button();
+            button3 = new Button();
+            button4 = new Button();
+            button5 = new Button();
+            button6 = new Button();
+            button7 = new Button();
+            button8 = new Button();
+            button9 = new Button();
+            button10 = new Button();
+            button11 = new Button();
+            button12 = new Button();
             SuspendLayout();
             // 
             // button1
@@ -37,6 +47,8 @@
             button1.BackColor = Color.FromArgb(65, 65, 65);
             button1.FlatAppearance.BorderSize = 0;
             button1.FlatStyle = FlatStyle.Flat;
+            button1.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            button1.ForeColor = Color.WhiteSmoke;
             button1.Location = new Point(12, 374);
             button1.Name = "button1";
             button1.Size = new Size(75, 75);
@@ -49,12 +61,154 @@
             button2.BackColor = Color.FromArgb(65, 65, 65);
             button2.FlatAppearance.BorderSize = 0;
             button2.FlatStyle = FlatStyle.Flat;
+            button2.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            button2.ForeColor = Color.WhiteSmoke;
             button2.Location = new Point(93, 374);
             button2.Name = "button2";
             button2.Size = new Size(75, 75);
             button2.TabIndex = 2;
-            button2.Text = "+/-";
+            button2.Text = "0";
             button2.UseVisualStyleBackColor = false;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.FromArgb(65, 65, 65);
+            button3.FlatAppearance.BorderSize = 0;
+            button3.FlatStyle = FlatStyle.Flat;
+            button3.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            button3.ForeColor = Color.WhiteSmoke;
+            button3.Location = new Point(174, 374);
+            button3.Name = "button3";
+            button3.Size = new Size(75, 75);
+            button3.TabIndex = 3;
+            button3.Text = ",";
+            button3.UseVisualStyleBackColor = false;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.FromArgb(65, 65, 65);
+            button4.FlatAppearance.BorderSize = 0;
+            button4.FlatStyle = FlatStyle.Flat;
+            button4.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            button4.ForeColor = Color.WhiteSmoke;
+            button4.Location = new Point(12, 293);
+            button4.Name = "button4";
+            button4.Size = new Size(75, 75);
+            button4.TabIndex = 4;
+            button4.Text = "1";
+            button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            button5.BackColor = Color.FromArgb(65, 65, 65);
+            button5.FlatAppearance.BorderSize = 0;
+            button5.FlatStyle = FlatStyle.Flat;
+            button5.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            button5.ForeColor = Color.WhiteSmoke;
+            button5.Location = new Point(93, 293);
+            button5.Name = "button5";
+            button5.Size = new Size(75, 75);
+            button5.TabIndex = 5;
+            button5.Text = "2";
+            button5.UseVisualStyleBackColor = false;
+            // 
+            // button6
+            // 
+            button6.BackColor = Color.FromArgb(65, 65, 65);
+            button6.FlatAppearance.BorderSize = 0;
+            button6.FlatStyle = FlatStyle.Flat;
+            button6.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            button6.ForeColor = Color.WhiteSmoke;
+            button6.Location = new Point(174, 293);
+            button6.Name = "button6";
+            button6.Size = new Size(75, 75);
+            button6.TabIndex = 6;
+            button6.Text = "3";
+            button6.UseVisualStyleBackColor = false;
+            // 
+            // button7
+            // 
+            button7.BackColor = Color.FromArgb(65, 65, 65);
+            button7.FlatAppearance.BorderSize = 0;
+            button7.FlatStyle = FlatStyle.Flat;
+            button7.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            button7.ForeColor = Color.WhiteSmoke;
+            button7.Location = new Point(12, 212);
+            button7.Name = "button7";
+            button7.Size = new Size(75, 75);
+            button7.TabIndex = 7;
+            button7.Text = "4";
+            button7.UseVisualStyleBackColor = false;
+            // 
+            // button8
+            // 
+            button8.BackColor = Color.FromArgb(65, 65, 65);
+            button8.FlatAppearance.BorderSize = 0;
+            button8.FlatStyle = FlatStyle.Flat;
+            button8.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            button8.ForeColor = Color.WhiteSmoke;
+            button8.Location = new Point(93, 212);
+            button8.Name = "button8";
+            button8.Size = new Size(75, 75);
+            button8.TabIndex = 8;
+            button8.Text = "5";
+            button8.UseVisualStyleBackColor = false;
+            // 
+            // button9
+            // 
+            button9.BackColor = Color.FromArgb(65, 65, 65);
+            button9.FlatAppearance.BorderSize = 0;
+            button9.FlatStyle = FlatStyle.Flat;
+            button9.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            button9.ForeColor = Color.WhiteSmoke;
+            button9.Location = new Point(174, 212);
+            button9.Name = "button9";
+            button9.Size = new Size(75, 75);
+            button9.TabIndex = 9;
+            button9.Text = "6";
+            button9.UseVisualStyleBackColor = false;
+            // 
+            // button10
+            // 
+            button10.BackColor = Color.FromArgb(65, 65, 65);
+            button10.FlatAppearance.BorderSize = 0;
+            button10.FlatStyle = FlatStyle.Flat;
+            button10.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            button10.ForeColor = Color.WhiteSmoke;
+            button10.Location = new Point(12, 131);
+            button10.Name = "button10";
+            button10.Size = new Size(75, 75);
+            button10.TabIndex = 10;
+            button10.Text = "7";
+            button10.UseVisualStyleBackColor = false;
+            // 
+            // button11
+            // 
+            button11.BackColor = Color.FromArgb(65, 65, 65);
+            button11.FlatAppearance.BorderSize = 0;
+            button11.FlatStyle = FlatStyle.Flat;
+            button11.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            button11.ForeColor = Color.WhiteSmoke;
+            button11.Location = new Point(93, 131);
+            button11.Name = "button11";
+            button11.Size = new Size(75, 75);
+            button11.TabIndex = 11;
+            button11.Text = "8";
+            button11.UseVisualStyleBackColor = false;
+            // 
+            // button12
+            // 
+            button12.BackColor = Color.FromArgb(65, 65, 65);
+            button12.FlatAppearance.BorderSize = 0;
+            button12.FlatStyle = FlatStyle.Flat;
+            button12.Font = new Font("Segoe UI Semibold", 15.75F, FontStyle.Bold);
+            button12.ForeColor = Color.WhiteSmoke;
+            button12.Location = new Point(174, 131);
+            button12.Name = "button12";
+            button12.Size = new Size(75, 75);
+            button12.TabIndex = 12;
+            button12.Text = "9";
+            button12.UseVisualStyleBackColor = false;
             // 
             // CalculatorForm
             // 
@@ -62,6 +216,16 @@
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = Color.FromArgb(35, 35, 35);
             ClientSize = new Size(384, 461);
+            Controls.Add(button12);
+            Controls.Add(button11);
+            Controls.Add(button10);
+            Controls.Add(button9);
+            Controls.Add(button8);
+            Controls.Add(button7);
+            Controls.Add(button6);
+            Controls.Add(button5);
+            Controls.Add(button4);
+            Controls.Add(button3);
             Controls.Add(button2);
             Controls.Add(button1);
             Name = "CalculatorForm";
@@ -72,5 +236,15 @@
         #endregion
         private Button button1;
         private Button button2;
+        private Button button3;
+        private Button button4;
+        private Button button5;
+        private Button button6;
+        private Button button7;
+        private Button button8;
+        private Button button9;
+        private Button button10;
+        private Button button11;
+        private Button button12;
     }
 }
